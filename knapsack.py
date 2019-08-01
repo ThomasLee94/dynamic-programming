f335b43000666f6a0e5a2006b833486b669ad897
Thomas <thomaslee9474@gmail.com>
2019-08-01 13:28:30 -0700

    [init]

---
+++ knapsack.py
@@ -0,0 +1,12 @@
+# For this input:
+#     List of items with size and value:
+#     [[size1, value1],[size2,value2 ],.....[size10, value10]]
+#     Size of knapsack: S
+
+# The solution to the knapsack problem is to take these items
+#     [[size3, value3], [size5,value5]]
+
+
+def value(bag_capacity, item_value, item_weight):
+    pass
+
