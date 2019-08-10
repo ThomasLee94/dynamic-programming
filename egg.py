eb2e6435e760a8b35e97fb9746e705da6ab77aee
Thomas <thomaslee9474@gmail.com>
2019-08-10 13:28:43 -0700

    [changes]

--- egg.py
+++ egg.py
@@ -1,30 +1,32 @@
-def drop_egg(total_floors: int, floor_num: int, egg_num: int, drops_num=0) -> int:
+def drop_egg(total_floors: int, num_eggs: int) -> int:
     """
         This function returns the minimum number of
-        drops needed to guarentee that the pivotal 
+        drops needed to guarentee that the pivotal
         floor (where the egg drops) is found.
     """
 
     # === BASE CASES ===
-    # case: no floors
-    if floor_num == 0:
-        return floor_num
-    # case: no eggs
-    if egg_num == 0:
-        return egg_num
-    # case: only 1 floor
-    if egg_num == 1:
+    # case: no floors or only 1 floor
+    if total_floors in (0, 1):
         return total_floors
+    # case: no eggs
+    if num_eggs == 0:
+        return num_eggs
     # case: only 1 floor
-    if floor_num == 1:
-        return floor_num
+    if num_eggs == 1:
+        return total_floors
 
-    # === DECISION TREE ===
-    # egg breaks
-    broken_egg = drop_egg(total_floors, floor_num - 1, egg_num - 1, drops_num)
-    # egg does not break
-    egg = drop_egg(total_floors, floor_num + 1, egg_num, drops_num)
+    # for every floor
+    for floor in total_floors:
+        # === DECISION TREE ===
+        # egg breaks
+        broken_egg = drop_egg(total_floors - 1, num_eggs - 1)
+        # egg does not break
+        egg = drop_egg(total_floors + 1, num_eggs)
 
-    drops_nums = max(broken_egg, egg)
-    
-    return drops_num
+        drops_num = max(broken_egg, egg)
+
+        return drops_num
+
+
+print(drop_egg(100, 2))
