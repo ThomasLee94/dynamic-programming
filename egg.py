361290564ed6f9dfb1b2926e08edc0772f6f345f
Thomas Lee <thomaslee9474@gmail.com>
2019-08-10 16:31:09 -0700

    [update] egg

--- egg.py
+++ egg.py
@@ -16,10 +16,16 @@ def drop_egg(total_floors: int, total_eggs: int, memoized_list=None) -> int:
         # G   3  0   1   2   2   3  
         # G   4  0   1   2   2   3  
         # S   5  0   1   2   2   3  
+    
+    # {
+    #   floor_num: {
+    #                egg_num: priority 
+    #               }
+    # }
 
+    infinity = math.inf
     # === INIT MEMOIZED 2D LIST ===
     if memoized_list is None:
-        infinity = math.inf
         memoized_list = list()
         for i in range(total_floors):
             memoized_list.append([])
@@ -28,36 +34,41 @@ def drop_egg(total_floors: int, total_eggs: int, memoized_list=None) -> int:
                 # infinity will be used for easy value comparison
                 memoized_list[i][j] = infinity
     
-    # === BASE CASES ===
-    # case: no floors or only 1 floor
-    if total_floors in (0, 1):
-        return total_floors
-    # case: no eggs
-    if total_eggs == 0:
-        return total_eggs
-    # case: only 1 floor
-    if total_eggs == 1:
-        return total_floors
+    # if memoized values already exist 
+    if memoized_list[total_floors-1][total_eggs-1] != infinity:
+        return memoized_list[total_floors][total_eggs]
 
-    # === FOR EVERY FLOOR ===
-    for floor in range(total_floors):
-        # === DECISION TREE ===
-        # case: egg breaks
-        broken_egg = drop_egg(total_floors - 1, total_eggs - 1, memoized_list)
-        # case: egg does not break
-        egg = drop_egg(total_floors + 1, total_eggs, memoized_list)
+    else:
+        # === BASE CASES ===
+        # case: no floors or only 1 floor
+        if total_floors in (0, 1):
+            return total_floors
+        # case: no eggs
+        if total_eggs == 0:
+            return total_eggs
+        # case: only 1 floor
+        if total_eggs == 1:
+            return total_floors
 
-        worst_case_drops_num = max(broken_egg, egg)
-        # +1 because because we want to account for the current floor
-        worst_case_drops_num + 1
+        # === FOR EVERY FLOOR ===
+        for floor in range(total_floors):
+            # === DECISION TREE ===
+            # case: egg breaks
+            broken_egg = drop_egg(total_floors - 1, total_eggs - 1, memoized_list)
+            # case: egg does not break
+            egg = drop_egg(total_floors + 1, total_eggs, memoized_list)
+
+            worst_case_drops_num = max(broken_egg, egg)
+            # +1 because because we want to account for the current floor
+            worst_case_drops_num + 1
+            
+            # update memoization
+            memoized_list[floor-1][total_eggs-1] = min(worst_case_drops_num, memoized_list[floor-1][total_eggs-1])
         
-        # update memoization
-        memoized_list[floor][total_eggs] = min(worst_case_drops_num, memoized_list[floor][total_eggs])
-    
-    return memoized_list[total_floors][total_eggs]
+        return memoized_list[total_floors-1][total_eggs-1]
         
 
-print(drop_egg(5, 2))
+drop_egg(5,2)
 
 # def stuff(total_floors, total_eggs):
 #     # INIT MEMOIZED 2D LIST
